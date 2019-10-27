57433c596d9fae6c25c57cf2af9ef8ba367695cf
Caloyski <cagflores@gmail.com>
2019-10-27 15:18:37 +0100

    Created Model Project

---
+++ Browzy/Browzy.Model/Entities/MembershipEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Browzy.Model.Entities {
+
+    public enum MembershipEnum {
+        Book,
+        Video,
+        Premium
+    }
+}
---
+++ Browzy/Browzy.Model/Entities/PurchaseOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Browzy.Model.Entities {
+
+    public class PurchaseOrder {
+
+        public int ID { get; set; }
+        public float Total { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
